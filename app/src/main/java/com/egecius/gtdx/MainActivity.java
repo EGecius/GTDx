3a48f2020d9c292591355a6a072bde1de83c5503
Egis <e.gecius@gmail.com>
2017-02-05 12:38:32 +0000

    add exploratory code

--- app/src/main/java/com/egecius/gtdx/MainActivity.java
+++ app/src/main/java/com/egecius/gtdx/MainActivity.java
@@ -1,13 +1,26 @@
 package com.egecius.gtdx;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.egecius.gtdx.datatypes.TodoTask;
+import com.egecius.gtdx.db.DatabaseExploration;
+import com.egecius.gtdx.db.Db;
+import com.egecius.gtdx.db.DbImpl;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+	private DatabaseExploration databaseExploration = new DatabaseExploration();
+
+	private Db db = new DbImpl(FirebaseDatabase.getInstance());
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		db.addTask(new TodoTask("id2", "egis task 2"));
 	}
+
 }
